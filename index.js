d656c63a1b7725cd631731b6f14e71eefbca1171
Talles Dantas <talles.soares@dreamstaffing.com>
2015-07-04 18:28:47 -0300

    Handlebars adicionado

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
+var hbs = require('express-hbs');
+var fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+  res.render('index');
 });
+
 
 var server = app.listen(3000, function () {
 
@@ -13,4 +22,3 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
-
